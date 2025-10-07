dbbfee219895859b66d75cbb12996ef1bc3cfe2d
QuangDaoTechnology <alvintrantechnology@gmail.com>
2025-10-07 23:53:12 +0700

    Make Express serverless for Vercel

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//serve static files from public
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
